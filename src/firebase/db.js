ffd86c72e1adb75addd4c405b997c1d050dafeb2
petr mikes <248622@vutbr.cz>
2026-06-15 16:57:33 +0200

    prepsani prokleti pismene p pouze pocinajici pismenem P

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -43,7 +43,7 @@ export const CURSES = [
   { id: 'oralni',       rarity: 'common', icon: '🤐', name: 'Orální kletba',              desc: 'Musíš být potichu. Pokud promluvíš, vylosuje se ti jedna z legendary kleteb.' },
   { id: 'chameleon',    rarity: 'common', icon: '🦎', name: 'Chameleoní kletba',          desc: 'Musíš být skrytý/á za větví.' },
   { id: 'superhvezda',  rarity: 'common', icon: '🖊️', name: 'Kletba superhvězdy',         desc: 'Musíš se podepsat na lidi, se kterými vedeš konverzaci.' },
-  { id: 'mluveni_p',   rarity: 'common', icon: '🅿️', name: 'Kletba mluvení po P',         desc: 'Musíš se dorozumívat jen slovy začínajícími na P.' },
+  { id: 'prokleti_p',   rarity: 'common', icon: '🅿️', name: 'Prokletí písmene P',          desc: 'Pán/paní promlouvá pouze pomocí pojmů počínající písmenem P' },
   { id: 'prazska',     rarity: 'common', icon: '🏙️', name: 'Pražská kletba',               desc: 'Hele koukej, právě jakoby teďkonc mluvíš jako Pražák viď.' },
   { id: 'hubeny_krivak',   rarity: 'common', icon: '🎭', name: 'Kletba hubeného křiváka',          desc: 'Musíš mluvit v rýmech.' },
 
